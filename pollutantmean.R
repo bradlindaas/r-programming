3a31c788bb46bd17c2e6b896d96ebd23d1e7f069
bradlindaas <brad@lindaas.org>
2014-05-08 22:46:59 -0500

    Finished #2, starting #3

--- pollutantmean.R
+++ pollutantmean.R
@@ -15,3 +15,22 @@ pollutantmean <- function (directory = "specdata",
     m<-mean(df[,pollutant], na.rm=T)
     print(m)
 }
+
+complete <- function(directory, id=1:332) {
+
+    df <- data.frame(id = numeric(0), nobs = numeric(0))
+    for (i in id) {
+        num <- sprintf("%03d", i)
+        path <- paste("assign-1/", directory, "/", num, ".csv", sep="")    
+        data <- read.csv(path)
+        cc <- sum(complete.cases(data))
+        row <- data.frame(id = i, nobs = cc)
+        df <- rbind(df, row)
+    }
+    return(df)
+}
+
+corr <- function(directory = "specdata", threshold = 0) {
+    
+}
+
