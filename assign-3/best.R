4df283f794153ba26a81caa49a48c62deeb926e2
bradlindaas <brad@lindaas.org>
2014-05-14 22:17:53 -0500

    Second Commit

--- assign-3/best.R
+++ assign-3/best.R
@@ -1,6 +1,23 @@
 best <- function(state, outcome) {
     ## Read outcome data
+    ## [,7] is state
+    ## [,11] is mortality rate of heart attack
+    ## [,17] is mortality rate of heart failure
+    ## [,23] is mortality rate of pneumonia
+    
+    data <-read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    data[,7]<-(as.factor(data[,7])) ## make State a factor
+    
     ## Check that state and outcome are valid
+    validStates <- levels(data)
+    
+    if (state %in% levels(data[,7])) {} else {
+        stop("invalid state")
+    }
+    
+    
+    
+    
     ## Return hospital name in that state with lowest 30-day death
     ## rate
 }
--- assign-3/rankall.R
+++ assign-3/rankall.R
@@ -1,5 +1,7 @@
 rankall <- function(outcome, num = "best") {
     ## Read outcome data
+    outcome<-read.csv("outcome-of-care-measures.csv", colClasses = "character")
+    
     ## Check that state and outcome are valid
     ## For each state, find the hospital of the given rank
     ## Return a data frame with the hospital names and the
