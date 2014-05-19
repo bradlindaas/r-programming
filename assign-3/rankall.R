8fdd1ccb4e0a1c609531d079ebd1d27aeac895d2
bradlindaas <brad@lindaas.org>
2014-05-19 15:33:21 -0500

    mass update

--- assign-3/rankall.R
+++ assign-3/rankall.R
@@ -1,12 +1,14 @@
 rankall <- function(outcome, num = "best") {
-    df <- data.frame(hospital = character(), state = factor())
-    source("rankhospital.R")
-    source("sort.R")
-    validStates <- getValidStates()
-    for (state in validStates){
-        hospital <- rankhospital(state, outcome, num)
-        row <- data.frame(hospital = hospital, state = state)
-        df <- rbind(df, row)
-    }
-    return(df)
+    source("helpFunctions.R")
+    data <- readData()
+    validStates<-getValidStates(data)
+    if (validateOutcome(outcome) == FALSE) stop("invalid outcome")
+    data <- sortByOutcome(data, outcome)
+    splitData <- split(data,data$state)
+    x <- lapply(splitData, getRank, outcome=outcome, num=num)
+    i <- 0
+    x <- lapply(x, function(x) {i=i+1 ; append(x, validStates[i])})
+    x <- do.call(rbind, x)
+    #x <- names(c("hospital", "state"))
+    return(x)
 }
